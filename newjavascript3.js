0cf5f2d2c545fd6f6273209d74aa371bb88c4715
moman64 <75200905+moman64@users.noreply.github.com>
2021-01-29 21:05:09 +0200

    Add files via upload

--- newjavascript3.js
+++ newjavascript3.js
@@ -1,2 +1,27 @@
+import * as importModule from "./newjavascript4";
+const $eventTitle = document.getElementsByClassName("inputName");
+const $eventDate = document.getElementsByClassName("inputDate");
+const $startBtn = document.getElementsByClassName("startbtn");
+const $stopBtn = document.getElementsByclassName("stopbtn");
+const $result = document.getElementsByClassName("result");
+$stopBtn.classList.add("disabled");
+
+        $eventTitle.classList.add("disabled");
+    $eventDate.classList.add("disabled");
+    $startBtn.classList.add("disabled");
+    $stopBtn.classList.remove("disabled");
+    $stopBtn.addEventListener('click',function () {
+        clearInterval(x);
+        $eventTitle.classList.remove("disabled");
+        $eventDate.classList.remove("disabled");
+        $startBtn.classList.remove("disabled");
+        $stopBtn.classList.add("disabled");
+    });
 
 
+$startBtn.addEventListener('click',function () {
+    if ($evTitle.value!=""&&$evDate.value!="") {
+        startCount();
+    }
+    else {alert("Please, Fill all the fields");}
+});
--- newjavascript4.js
+++ newjavascript4.js
@@ -1,7 +1,24 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+export function startEvent () {
+    var x = setInterval(function() {
+        let evTitle = $eventTitle.value;
+        let evDate = new Date($eventDate.value).getTime();
+        let nowDate = new Date().getTime();
+        let toDate = evDate-nowDate;
+        if (toDate>=0) {
+            var days = Math.floor(toDate / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((toDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((toDate % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((toDate % (1000 * 60)) / 1000);
+            
+            $result.innerHTML =`The Event ${evTitle} will start after <br>
+            ${days} d ${hours} h ${minutes} m ${seconds} s`;
+        }
+        else {
+            alert("Already Passed");
+            clearInterval(x);
+            $evTitle.classList.remove("disabled");
+            $evDate.classList.remove("disabled");
+            $startBtn.classList.remove("disabled");
+            $stopBtn.classList.add("disabled");
+    }},1000);
 
